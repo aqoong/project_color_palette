6ec5e8960681331abc21f606f3edc884e61467ea
wonwoohan <cooldnjsdn@gmail.com>
2024-09-11 01:17:19 +0900

    add example result color dart

---
+++ example/lib/palette/color_test.g.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+/// Auto-generated from project_color_palette package.
+/// From 'color_test.csv'
+/// color row length 1
+class ColorTest {
+  static const Color colorWhite = Color(0xFFFFFFFF);  //Comment test
+}
